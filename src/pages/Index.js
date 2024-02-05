5366c5911e795ee6a50e0e542e8eaad907445b3a
Riccardo Rizzo <riccardo.rizzo@machinemax.com>
2024-02-05 08:58:20 +0000

    Makefile & code fix

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -34,8 +34,7 @@ const Index = () => (
         Welcome to my website. Please feel free to read more{" "}
         <Link to="/about">about me</Link>, or you can check out my{" "}
         <Link to="/resume">resume</Link>, <Link to="/projects">projects</Link>,{" "}
-        <Link to="/pubclications">publications</Link>, view{" "}
-        <Link to="/stats">site statistics</Link>, or{" "}
+        <Link to="/publications">publications</Link> or{" "}
         <Link to="/contact">contact</Link> me.
       </p>
     </article>
